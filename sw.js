9cc761fdc00a94c33c0a149d3e268a91dd422c4c
novoselovalexandr1988-ctrl <novoselov.alexandr.1988@gmail.com>
2026-03-17 17:00:25 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
 
+self.addEventListener('activate', function(e) {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('notificationclick', function(e) {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('/stesha/'));
+});
