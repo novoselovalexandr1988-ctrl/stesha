4d3405deb64f9b5d098c05212e5b8f66c25a8e20
novoselovalexandr1988-ctrl <novoselov.alexandr.1988@gmail.com>
2026-03-18 10:08:44 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+var CACHE_NAME = 'stesha-v2';
+
 self.addEventListener('install', function(e) {
   self.skipWaiting();
 });
@@ -8,5 +10,26 @@ self.addEventListener('activate', function(e) {
 
 self.addEventListener('notificationclick', function(e) {
   e.notification.close();
-  e.waitUntil(clients.openWindow('/stesha/'));
+  e.waitUntil(
+    clients.matchAll({type:'window',includeUncontrolled:true}).then(function(list){
+      for(var i=0;i<list.length;i++){
+        if(list[i].url.indexOf('stesha')!==-1 && 'focus' in list[i]) return list[i].focus();
+      }
+      if(clients.openWindow) return clients.openWindow('/stesha/');
+    })
+  );
+});
+
+self.addEventListener('push', function(e) {
+  var data = e.data ? e.data.json() : {};
+  e.waitUntil(
+    self.registration.showNotification(data.title || 'Стеша', {
+      body: data.body || '',
+      icon: 'https://img.icons8.com/?size=192&id=Sawg2D8Sr9wA&format=png&color=000000',
+      badge: 'https://img.icons8.com/?size=96&id=Sawg2D8Sr9wA&format=png&color=000000',
+      vibrate: [200, 100, 200],
+      tag: 'stesha-notify',
+      renotify: true
+    })
+  );
 });
